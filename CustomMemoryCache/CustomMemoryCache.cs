a054f6439bd55679ffe4884e5f4f65d0d5103a14
“Taylor <t4ybennett@gmail.com>
2023-10-09 17:48:57 +0100

    Add requirements

--- CustomMemoryCache/CustomMemoryCache.cs
+++ CustomMemoryCache/CustomMemoryCache.cs
@@ -1,4 +1,15 @@
 ﻿namespace CustomMemoryCache;
+
+#region Requirements
+
+// create a generic in-memory cache component, which other developers can use in their applications.
+// component should be able to store arbitrary types of objects, which are added and retrieved using a unique key (similar to a dictionary).
+// configurable threshold for max number of items, if full it should evict a least used item to make space for a new item
+// should be thread-safe singleton
+// notify consumer if items evicted
+
+#endregion
+
 public static class CustomMemoryCache
 {
     public static int AddIntegers(int a, int b) {
