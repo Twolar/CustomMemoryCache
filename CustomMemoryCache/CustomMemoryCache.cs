0c90aab716176e1373e9136a4e54fb7d48fc9b42
“Taylor <t4ybennett@gmail.com>
2023-10-16 19:22:48 +0100

    Initial solution + base tests

--- CustomMemoryCache/CustomMemoryCache.cs
+++ CustomMemoryCache/CustomMemoryCache.cs
@@ -2,23 +2,112 @@
 
 #region Requirements
 
-// create a generic in-memory cache component, which other developers can use in their applications.
-// component should be able to store arbitrary types of objects, which are added and retrieved using a unique key (similar to a dictionary).
-// configurable threshold for max number of items, if full it should evict a least used item to make space for a new item
-// should be thread-safe singleton
-// notify consumer if items evicted
+// [] create a generic in-memory cache component, which other developers can use in their applications.
+// [] component should be able to store arbitrary types of objects, which are added and retrieved using a unique key (similar to a dictionary).
+// [] configurable threshold for max number of items, if full it should evict a least used item to make space for a new item
+// [] should be thread-safe singleton
+// [] notify consumer if items evicted
 
 #endregion
 
-public class CustomMemoryCache<TKey, TValue>
+// TODO: Double check data structure layout
+// TODO: Unique key, some sort of hashing?
+// TODO: Singleton / Thread safe tests
+// TODO: Notify consumer of items evicted
+// TODO: Investigate compiler warnings regarding null references
+
+public class CustomMemoryCache<TKey>
 {
-    // Properties
+    private static readonly object padlock = new object();
+    private static CustomMemoryCache<TKey> instance = null;
 
-    // Constructor
+    private readonly int _cacheCapacity;
+    private readonly Dictionary<TKey, LinkedListNode<CacheItem>> _cache;
+    private readonly LinkedList<CacheItem> _lruList;
 
-    // Add
+    private CustomMemoryCache(int size = 5)
+    {
+        _cacheCapacity = size;
+        _cache = new Dictionary<TKey, LinkedListNode<CacheItem>>(_cacheCapacity);
+        _lruList = new LinkedList<CacheItem>();
+    }
 
-    // Get
+    public static void Initialize(int size)
+    {
+        lock (padlock)
+        {
+            if (instance == null)
+            {
+                instance = new CustomMemoryCache<TKey>(size);
+            }
+        }
+    }
 
-    // Remove
+    public static CustomMemoryCache<TKey> Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    throw new InvalidOperationException("Must call Initialize before using this cache");
+                }
+                return instance;
+            }
+        }
+    }
+
+    public bool Add(TKey key, object value)
+    {
+        if (_cache.Count >= _cacheCapacity)
+        {
+            RemoveLeastUsed();
+        }
+
+        // Duplicate Key Check
+        if (_cache.TryGetValue(key, out var duplicateNode))
+        {
+            throw new Exception("The key already exists in the cache");
+        }
+
+        var cacheItem = new CacheItem { CIKey = key, CIValue = value };
+        var node = new LinkedListNode<CacheItem>(cacheItem);
+
+        // Duplicate Value Check
+        if (_cache.Select(kvp => kvp.Value.Value)
+            .Any(cacheItem => cacheItem.CIValue.Equals(value)))
+        {
+            throw new Exception($"The value already exists in the cache");
+        }
+
+        _lruList.AddFirst(node);
+        _cache.Add(key, node);
+
+        return true;
+    }
+
+    public object Get(TKey key)
+    {
+        if (_cache.TryGetValue(key, out var node))
+        {
+            _lruList.Remove(node);
+            _lruList.AddFirst(node);
+            return node.Value.CIValue;
+        }
+        throw new KeyNotFoundException($"The key {key} was not found in the store.");
+    }
+
+    private void RemoveLeastUsed()
+    {
+        var lastNode = _lruList.Last;
+        _cache.Remove(lastNode.Value.CIKey);
+        _lruList.RemoveLast();
+    }
+
+    private class CacheItem
+    {
+        public TKey CIKey { get; set; }
+        public object CIValue { get; set; }
+    }
 }
--- CustomMemoryCacheTest/Tests.cs
+++ CustomMemoryCacheTest/Tests.cs
@@ -7,8 +7,65 @@ public class Tests
     // Act
     // Assert
 
+    private CustomMemoryCache<string> cache;
+
     [SetUp]
-    public void Setup()
+    public void SetUp()
     {
+        CustomMemoryCache<string>.Initialize(3);
+        cache = CustomMemoryCache<string>.Instance;
+    }
+
+    [Test]
+    public void AddAndGet()
+    {
+        var key = "ANG-item1";
+        var value = "ANG-value1";
+        cache.Add(key, value);
+        Assert.That(cache.Get(key), Is.EqualTo(value));
+    }
+
+    [Test]
+    public void AddDoesNotAddDuplicateKeyToCache()
+    {
+        var duplicateKey = "ADNADKTC-item1";
+
+        cache.Add(duplicateKey, "ADNADKTC-value1");
+
+        var exception = Assert.Throws<Exception>(() => cache.Add(duplicateKey, "ADNADKTC-value2"));
+        Assert.That(exception.Message, Is.EqualTo("The key already exists in the cache"));
+    }
+
+    [Test]
+    public void AddDoesNotAddDuplicateObjectValueToCache()
+    {
+        var duplicateValue = "ADNADOVTC-value1";
+
+        cache.Add("ADNADOVTC-item1", duplicateValue);
+
+        var exception = Assert.Throws<Exception>(() => cache.Add("ADNADOVTC-item2", duplicateValue));
+        Assert.That(exception.Message, Is.EqualTo("The value already exists in the cache"));
+    }
+
+    [Test]
+    public void GetThrowsExceptionWhenKeyDoesNotExist()
+    {
+        var keyThatDoesNotExist = "GTEWKDNE-item1";
+        var exception = Assert.Throws<KeyNotFoundException>(() => cache.Get(keyThatDoesNotExist));
+        Assert.That(exception.Message, Is.EqualTo($"The key {keyThatDoesNotExist} was not found in the store."));
+    }
+
+    [Test]
+    public void RemoveLeastUsed()
+    {
+        var keyToBeEvictedFromCache = "RLU-item1";
+
+        cache.Add(keyToBeEvictedFromCache, "RLU-value1");
+        cache.Add("RLU-item2", "RLU-value2");
+        cache.Add("RLU-item3", "RLU-value3");
+        cache.Add("RLU-item4", "RLU-value4");
+
+        var exception = Assert.Throws<KeyNotFoundException>(() => cache.Get(keyToBeEvictedFromCache));
+        Assert.That(exception.Message, Is.EqualTo($"The key {keyToBeEvictedFromCache} was not found in the store."));
     }
 }
