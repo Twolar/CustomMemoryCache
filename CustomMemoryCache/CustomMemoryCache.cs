43314910cf0c8836253144d3762273d9da818038
“Taylor <t4ybennett@gmail.com>
2023-10-17 14:23:13 +0100

    Comment changes

--- CustomMemoryCache/CustomMemoryCache.cs
+++ CustomMemoryCache/CustomMemoryCache.cs
@@ -4,15 +4,13 @@ namespace CustomMemoryCache;
 
 #region Requirements
 
-// [] create a generic in-memory cache component, which other developers can use in their applications.
+// [x] create a generic in-memory cache component, which other developers can use in their applications.
 // [] component should be able to store arbitrary types of objects, which are added and retrieved using a unique key (similar to a dictionary).
-// [] configurable threshold for max number of items, if full it should evict a least used item to make space for a new item
-// [] should be thread-safe singleton
+// [x] configurable threshold for max number of items, if full it should evict a least used item to make space for a new item
+// [x] should be thread-safe singleton
 // [] notify consumer if items evicted
 
 #endregion
-
-// TODO: Notify consumer of items evicted
 
 public class CustomMemoryCache<TKey> where TKey : notnull
 {
